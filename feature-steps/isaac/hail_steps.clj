121051f7f813daad9f19a33bae7c09dc6abcc981
Zane <assistantmicahmartin@gmail.com>
2026-09-13 15:08:27 -0700

    Refresh config between hail CLI feature invocations
    Isaac-Session: isaac-work-1
    Isaac-Bean: isaac-8got

--- feature-steps/isaac/hail_steps.clj
+++ feature-steps/isaac/hail_steps.clj
@@ -8,6 +8,7 @@
     [gherclj.core :as g :refer [defgiven defwhen defthen helper!]]
     [isaac.agent.config.runtime :as agent-runtime]
     [isaac.drive.turn]
+    [isaac.config.agent-steps]
     [isaac.config.api :as config]
     [isaac.config.loader :as loader]
     [isaac.foundation.cli-steps :as fcli]
